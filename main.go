ff4d981cd005c1d989107be232934d3d6e908e3e
syossan27 <wisdom1027@gmail.com>
2016-02-11 23:47:28 +0900

    テンプレートの利用

--- main.go
+++ main.go
@@ -3,21 +3,31 @@ package main
 import (
   "log"
   "net/http"
+  "sync"
+  "text/template"
+  "path/filepath"
 )
 
-func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte(`
-      <html>
-        <head>
-          <title>チャット</title>
-        </head>
-        <body>
-          チャットしましょう！
-        </body>
-      </html>
-    `))
+// templは１つのテンプレートを表します
+type templateHandler struct {
+  once sync.Once
+  filename string
+  templ *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  // このonce.Doについてはこのページ参照のこと
+  // http://golang.jp/pkg/sync
+  t.once.Do(func() {
+    t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
   })
+  t.templ.Execute(w, nil)
+}
+
+func main() {
+  // ルート
+  http.Handle("/", &templateHandler{filename: "chat.html"})
 
   // Webサーバーを開始します
   if err := http.ListenAndServe(":8080", nil); err != nil {
